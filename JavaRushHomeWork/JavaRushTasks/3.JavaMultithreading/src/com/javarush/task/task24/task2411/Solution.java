f668cdc65f26508b951df012d84d2f966f999e44
Ilya Shebanov <ivshebanov@gmail.com>
2017-03-04 00:36:32 +0300

    задача решена package com.javarush.task.task24.task2411;

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2411/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2411/Solution.java
@@ -1,19 +1,19 @@
 package com.javarush.task.task24.task2411;
 
 /* 
-Вспомним наследование
+Вспомним наследование CCBAYS
 */
-public class Solution {
+
+public class Solution extends C{
     private class A {
         protected String value = "A";
-
         public A() {
             System.out.print(value);
         }
     }
 
     private A a = new A() {
-        { //у анонимных классов нет своих конструкторов, но что-то можно сделать в блоке инициализации класса
+        {//у анонимных классов нет своих конструкторов, но что-то можно сделать в блоке инициализации класса
             value = "Y";
             if (super.getClass().getName().contains(".Solution$")) {
                 System.out.print(value);
@@ -29,6 +29,5 @@ public class Solution {
         new Solution();
     }
 }
-
 
 
