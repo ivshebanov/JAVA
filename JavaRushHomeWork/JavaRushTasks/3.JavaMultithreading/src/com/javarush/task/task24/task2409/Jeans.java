62ba22ddd1b7b1b806f01d0b303d7a8050caad2f
ivshebanov <ivshebanov@gmail.com>
2017-03-03 16:48:05 +0300

    Задача package com.javarush.task.task24.task2409; РЕШЕНА!

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Администратор on 03.03.2017.
+ */
+public interface Jeans extends Item {
+    int getLength();
+
+    int getSize();
+}
