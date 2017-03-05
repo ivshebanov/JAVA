d1ce71d4edbc322a98c6bbd26325a866ee58c724
Ilya Shebanov <ivshebanov@gmail.com>
2017-03-05 12:50:24 +0300

    MVC(2)

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.*;
+
+/**
+ * Created by iliashebanov on 05.03.17.
+ */
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User("A", 1, 1));
+        users.add(new User("B", 2, 2));
+        users.add(new User("C", 3, 3));
+        users.add(new User("D", 4, 4));
+        modelData.setUsers(users);
+    }
+}
---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import java.util.*;
+
+/**
+ * Created by iliashebanov on 05.03.17.
+ */
+public class ModelData {
+    private List<User> users = new ArrayList<User>();
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
