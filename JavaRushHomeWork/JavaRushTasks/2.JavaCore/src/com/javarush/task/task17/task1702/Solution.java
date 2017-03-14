d8d2a3bcdf4c77163ed351f4f233932f9155cfb3
ivshebanov <ivshebanov@gmail.com>
2017-03-14 13:03:07 +0300

    Синхронизированные заметки 2

--- JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1702/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1702/Solution.java
@@ -40,7 +40,7 @@ public class Solution {
         for (Thread thread : threads) thread.join();
     }
 
-    public static void sort(int[] array) {
+    public static synchronized void sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] < array[j]) {
@@ -49,6 +49,25 @@ public class Solution {
                     array[j] = k;
                 }
             }
+        }
+    }
+
+    public static class SortThread extends Thread {
+        /**
+         * If this thread was constructed using a separate
+         * <code>Runnable</code> run object, then that
+         * <code>Runnable</code> object's <code>run</code> method is called;
+         * otherwise, this method does nothing and returns.
+         * <p>
+         * Subclasses of <code>Thread</code> should override this method.
+         *
+         * @see #start()
+         * @see #stop()
+         * @see #Thread(ThreadGroup, Runnable, String)
+         */
+        @Override
+        public void run() {
+            sort(testArray);
         }
     }
 }
