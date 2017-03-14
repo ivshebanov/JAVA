3abed6eb483bd2ceeacd2fbd15dcaf5594d1bb09
ivshebanov <ivshebanov@gmail.com>
2017-03-14 12:33:00 +0300

    уровень 7, продолжение Тредов

---
+++ JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1701/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task17.task1701;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Заметки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new NoteThread().start();
+        new NoteThread().start();
+    }
+
+    public static class Note {
+        public static final List<String> notes = new ArrayList<String>();
+
+        public static synchronized void addNote(String note) {
+            notes.add(0, note);
+        }
+
+        public static synchronized void removeNote(String threadName) {
+            String note = notes.remove(0);
+            if (note == null) {
+                System.out.println("Другая нить удалила нашу заметку");
+            } else if (!note.startsWith(threadName)) {
+                System.out.println("Нить [" + threadName + "] удалила чужую заметку [" + note + "]");
+            }
+        }
+    }
+
+    public static class NoteThread extends Thread {
+        /**
+         * If this thread was constructed using a separate
+         * <code>Runnable</code> run object, then that
+         * <code>Runnable</code> object's <code>run</code> method is called;
+         * otherwise, this method does nothing and returns.
+         * <p>
+         * Subclasses of <code>Thread</code> should override this method.
+         *
+         * @see #start()
+         * @see #stop()
+         * @see #Thread(ThreadGroup, Runnable, String)
+         */
+        @Override
+        public void run() {
+            for (int index = 0; index < 1000; index++) {
+                Note.addNote(getName() + "-Note" + index);
+            }
+            for (int index = 0; index < 1000; index++) {
+                Note.removeNote(getName());
+            }
+        }
+    }
+
+}
