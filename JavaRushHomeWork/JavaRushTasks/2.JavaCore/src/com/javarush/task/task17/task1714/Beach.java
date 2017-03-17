0fdc6fc123555cb56cb8fdbcdc4346469988fc41
ivshebanov <ivshebanov@gmail.com>
2017-03-17 17:23:54 +0300

    Чем выше качество - тем лучше пляж. Чем больше дистанция - тем хуже пляж.

---
+++ JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -0,0 +1,102 @@
+package com.javarush.task.task17.task1714;
+
+/* 
+Comparable
+Чем выше качество - тем лучше пляж.
+Чем больше дистанция - тем хуже пляж.
+*/
+
+public class Beach implements Comparable<Beach> {
+    private String name;      //название
+    private float distance;   //расстояние
+    private int quality;    //качество
+
+    public Beach(String name, float distance, int quality) {
+        this.name = name;
+        this.distance = distance;
+        this.quality = quality;
+    }
+
+    public synchronized String getName() {
+        return name;
+    }
+
+    public synchronized void setName(String name) {
+        this.name = name;
+    }
+
+    public synchronized float getDistance() {
+        return distance;
+    }
+
+    public synchronized void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public synchronized int getQuality() {
+        return quality;
+    }
+
+    public synchronized void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public static void main(String[] args) {
+        Beach beach1 = new Beach("Mayami beach", 5, 10); //5
+        Beach beach2 = new Beach("Moscow beach", 5, 10); //6
+        System.out.println(beach1.compareTo(beach2));
+    }
+
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     * <p>
+     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+     * <tt>y.compareTo(x)</tt> throws an exception.)
+     * <p>
+     * <p>The implementor must also ensure that the relation is transitive:
+     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+     * <tt>x.compareTo(z)&gt;0</tt>.
+     * <p>
+     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+     * all <tt>z</tt>.
+     * <p>
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+     * class that implements the <tt>Comparable</tt> interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     * <p>
+     * <p>In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public synchronized int compareTo(Beach o) {
+        int beach1 = (int) -this.distance + this.quality;
+        int beach2 = (int) -o.distance + o.quality;
+        if (beach1 > beach2) {
+            return beach1 - beach2;
+        }
+        if (beach1 < beach2) {
+            return beach1 - beach2;
+        } else {
+            return 0;
+        }
+    }
+}
