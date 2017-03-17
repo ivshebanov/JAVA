a5b7f3e65cb9c117faaa9fcf38ba3302f4829bf7
ivshebanov <ivshebanov@gmail.com>
2017-03-17 17:50:16 +0300

    1

---
+++ JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1715/Solution.java
@@ -0,0 +1,80 @@
+package com.javarush.task.task17.task1715;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Аптека
+*/
+
+public class Solution {
+    public static DrugsController drugsController = new DrugsController();
+    public static boolean isStopped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread apteka = new Thread(new Apteka());
+        Thread man = new Thread(new Person(), "Мужчина");
+        Thread woman = new Thread(new Person(), "Женщина");
+
+        apteka.start();
+        man.start();
+        woman.start();
+
+        Thread.sleep(1000);
+        isStopped = true;
+    }
+
+    public static class Apteka implements Runnable{
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public static class Person implements Runnable{
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @Override
+        public void run() {
+        }
+    }
+
+    public static int getRandomCount() {
+        return (int) (Math.random() * 3) + 1;
+    }
+
+    public static Drug getRandomDrug() {
+        int index = (int) ((Math.random() * 1000) % (drugsController.allDrugs.size()));
+        List<Drug> drugs = new ArrayList<>(drugsController.allDrugs.keySet());
+        return drugs.get(index);
+    }
+
+    private static void waitAMoment() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+    }
+}
