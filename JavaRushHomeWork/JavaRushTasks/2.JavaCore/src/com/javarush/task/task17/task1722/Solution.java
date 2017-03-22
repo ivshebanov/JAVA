5570169067a4bf859803a2fcacff55b9bd250380
ivshebanov <ivshebanov@gmail.com>
2017-03-22 11:46:40 +0300

    задача на join

--- JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1722/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1722/Solution.java
@@ -11,10 +11,15 @@ public class Solution {
         Counter counter3 = new Counter();
         Counter counter4 = new Counter();
 
+
         counter1.start();
+        counter1.join();
         counter2.start();
+        counter2.join();
         counter3.start();
+        counter3.join();
         counter4.start();
+        counter4.join();
 
         for (int i = 1; i <= 100; i++) {
             if (values[i] != 1) {
@@ -45,7 +50,7 @@ public class Solution {
         @Override
         public void run() {
             do {
-                synchronized (this) {
+                synchronized (values) {
                     incrementCount();
                     values[getCount()]++;
                 }
