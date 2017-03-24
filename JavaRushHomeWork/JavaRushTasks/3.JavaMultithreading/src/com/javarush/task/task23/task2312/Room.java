9e180dbdbc2e73c5626de4d22810912a2576a26b
ivshebanov <ivshebanov@gmail.com>
2017-03-24 11:23:38 +0300

    Змейка (1)

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by Администратор on 24.03.2017.
+ */
+public class Room {
+    public static void main(String[] args){
+
+    }
+}
---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by Администратор on 24.03.2017.
+ */
+public class Snake {
+}
