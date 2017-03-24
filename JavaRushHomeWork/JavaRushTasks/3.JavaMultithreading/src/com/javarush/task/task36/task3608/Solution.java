48ac0c76f1db181b7445e889ef6e6867e620c457
ivshebanov <ivshebanov@gmail.com>
2017-03-24 10:29:53 +0300

    MVC(9) задача на MVC решена полностью

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -22,6 +22,7 @@ public class Solution {
         usersView.fireEventShowAllUsers();
         usersView.fireEventOpenUserEditForm(126);
         editUserView.fireEventUserDeleted(124L);
+        editUserView.fireEventUserChanged("Ivanov Ivan", 123, 10);
         usersView.fireEventShowDeletedUsers();
     }
 }
