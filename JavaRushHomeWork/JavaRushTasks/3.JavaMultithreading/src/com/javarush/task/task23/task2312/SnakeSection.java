7e70953a5d56067cea068f3024cef30b878a201c
ivshebanov <ivshebanov@gmail.com>
2017-03-24 12:26:03 +0300

    Змейка (6)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -4,4 +4,19 @@ package com.javarush.task.task23.task2312;
  * Created by Администратор on 24.03.2017.
  */
 public class SnakeSection {
+    private int x;
+    private int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
