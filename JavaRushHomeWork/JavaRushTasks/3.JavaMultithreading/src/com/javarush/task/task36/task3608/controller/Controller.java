04e800c974b0b93118fdea154c9f7224601f066c
ivshebanov <ivshebanov@gmail.com>
2017-04-05 10:13:56 +0300

    MVC(2)

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task36.task3608.controller;
+
+/**
+ * Created by Администратор on 04.04.2017.
+ */
+public class Controller {
+}
---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/View.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task36.task3608.view;
+
+/**
+ * Created by Администратор on 05.04.2017.
+ */
+public interface View {
+}
