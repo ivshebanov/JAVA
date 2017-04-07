a0d4b48a6d047f2c659b4f10c6a15e388b0172f3
ivshebanov <ivshebanov@gmail.com>
2017-04-07 13:47:19 +0300

    Находим все файлы. Решена с помощью Files.walkFileTree(....);

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,6 +1,13 @@
 package com.javarush.task.task31.task3102;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedList;
 import java.util.List;
 
 /* 
@@ -8,8 +15,20 @@ import java.util.List;
 */
 public class Solution {
     public static List<String> getFileTree(String root) throws IOException {
-        return null;
+        List<String> list = new LinkedList<String>();
 
+        try {
+            Files.walkFileTree(new File(root).toPath(), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    list.add(file.toFile().getAbsolutePath());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public static void main(String[] args) {
