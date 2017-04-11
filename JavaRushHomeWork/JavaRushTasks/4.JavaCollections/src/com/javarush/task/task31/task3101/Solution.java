29394b7f243d5a4ee6e79fe9aca36d589a86ced0
ivshebanov <ivshebanov@gmail.com>
2017-04-11 17:02:09 +0300

    3101 Проход по дереву файлов

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -11,68 +11,89 @@ import java.util.List;
 Проход по дереву файлов
 */
 public class Solution {
-    private static List<File> fileList = new ArrayList<File>();
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        File path = new File(args[0]);
+//        File resultFileAbsolutePath = new File(args[1]);
+//        if (!resultFileAbsolutePath.exists()) try {
+//            resultFileAbsolutePath.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        File allFilesContent = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
+//        FileUtils.renameFile(resultFileAbsolutePath, allFilesContent);
+//
+//        List<File> fileList = checkDirectory(path);
+//
+//        if (fileList.contains(allFilesContent)) {
+//            fileList.remove(allFilesContent);
+//        }
+//
+//        fileList.sort(Comparator.comparing(File::getName));
+//
+//
+//        try (FileOutputStream writer = new FileOutputStream(allFilesContent)) {
+//            for (File f : fileList) {
+//                try (BufferedReader reader = new BufferedReader(new FileReader(f));){
+//                    while (reader.ready()) {
+//                        writer.write(reader.read());
+//                    }
+//                    writer.write("\n".getBytes());
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         File path = new File(args[0]);
         File resultFileAbsolutePath = new File(args[1]);
-        try {
-            if (!resultFileAbsolutePath.exists()) resultFileAbsolutePath.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File allFilesContent = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
-        FileUtils.renameFile(resultFileAbsolutePath, allFilesContent);
-
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(allFilesContent, true));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        checkDirectory(path, allFilesContent);
-        //        fileList.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
-        fileList.sort(Comparator.comparing(File::getName));
-
-        try {
-            writFile(fileList, writer);
-            assert writer != null;
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void writFile(List<File> list, BufferedWriter writer) throws IOException {
-        for (File f : list) {
-            BufferedReader reader = new BufferedReader(new FileReader(f));
-            while (reader.ready()) {
-                writer.write(reader.readLine());
-                writer.newLine();
-            }
-            reader.close();
-        }
-    }
-
-    private static void checkDirectory(File f, File allFilesContent) {
-        try {
-            Files.walkFileTree(f.toPath(), new SimpleFileVisitor<Path>() {
+        File dest = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
+        FileUtils.renameFile(resultFileAbsolutePath, dest);
+        ArrayList<File> list = new ArrayList<>();
+        try (FileOutputStream writer = new FileOutputStream(dest)) {
+            Files.walkFileTree(path.toPath(), new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    if (file.toFile().length() > 50 && !file.toFile().getAbsoluteFile().equals(allFilesContent.getAbsoluteFile()))
+                    if (file.toFile().length() > 50) {
                         FileUtils.deleteFile(file.toFile());
-                    else if (!file.toFile().getAbsoluteFile().equals(allFilesContent.getAbsoluteFile())) {
-                        fileList.add(file.toFile());
+                    } else {
+                        list.add(file.toFile());
                     }
                     return FileVisitResult.CONTINUE;
                 }
             });
-        } catch (IOException e) {
-            e.printStackTrace();
+            list.sort(Comparator.comparing(File::getName));
+
+            for (File file : list) {
+                if (!file.equals(dest)) {
+                    FileReader reader = new FileReader(file);
+                    while (reader.ready()) writer.write(reader.read());
+                    reader.close();
+                    writer.write("\n".getBytes());
+                }
+            }
+            writer.close();
         }
     }
+
+//    private static List<File> checkDirectory(File f) {
+//        List<File> fileList = new ArrayList<File>();
+//        try {
+//            Files.walkFileTree(f.toPath(), new SimpleFileVisitor<Path>() {
+//                @Override
+//                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+//                    if (file.toFile().length() > 50) {
+//                        FileUtils.deleteFile(file.toFile());
+//                    } else {
+//                        fileList.add(file.toFile());
+//                    }
+//                    return FileVisitResult.CONTINUE;
+//                }
+//            });
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return fileList;
+//    }
 
     public static void deleteFile(File file) {
         if (!file.delete()) System.out.println("Can not delete file with name " + file.getName());
