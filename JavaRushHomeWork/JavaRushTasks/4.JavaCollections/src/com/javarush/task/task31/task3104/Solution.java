97264bd934f522f1c89af29c3d13f251c9cf98e2
ivshebanov <ivshebanov@gmail.com>
2017-04-12 15:41:06 +0300

    3104 Поиск скрытых файлов

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3104/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3104/Solution.java
@@ -10,12 +10,14 @@ import java.util.List;
 
 /* 
 Поиск скрытых файлов
+D:\Hotj\JAVA\JavaRushHomeWork\JavaRushTasks\4.JavaCollections\src\com\javarush\task\task31\task3101\test
 */
 public class Solution extends SimpleFileVisitor<Path> {
     public static void main(String[] args) throws IOException {
         EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
         final Solution solution = new Solution();
-        Files.walkFileTree(Paths.get("D:/"), options, 20, solution);
+//        Files.walkFileTree(Paths.get("D:/"), options, 20, solution);
+        Files.walkFileTree(Paths.get("D:\\Distributive\\Keil"), options, 20, solution);
 
         List<String> result = solution.getArchived();
         System.out.println("All archived files:");
@@ -39,5 +41,19 @@ public class Solution extends SimpleFileVisitor<Path> {
 
     public List<String> getFailed() {
         return failed;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (file.toString().endsWith(".rar") || file.toString().endsWith(".zip")) {
+            archived.add(file.toString());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        failed.add(file.toString());
+        return FileVisitResult.SKIP_SUBTREE;
     }
 }
