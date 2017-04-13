7552e0708821ed6f59c4a7d07d60eb8bca1c4fa0
ivshebanov <ivshebanov@gmail.com>
2017-04-13 18:29:52 +0300

    3105 Добавление файла в архив

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -1,12 +1,9 @@
 package com.javarush.task.task31.task3105;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -18,19 +15,44 @@ D:\Hotj\JAVA\JavaRushHomeWork\JavaRushTasks\4.JavaCollections\src\com\javarush\t
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-        ArrayList<ZipEntry> arrayList = new ArrayList<ZipEntry>();
+        if (args.length < 2)
+            return;
+        Map<ZipEntry, StringBuffer> mapZipEntry = new HashMap<>();
+
         try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(args[1]));
-             ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(args[1]));){
-            while (zipInputStream.available() != 0){
-                arrayList.add(zipInputStream.getNextEntry());
-        }
-            String el0 = args[0];
-            String s = "new\\" + el0.substring(el0.lastIndexOf("\\")+1);
-            Path dir = Files.createDirectories(Paths.get(s));
-            arrayList.add(new ZipEntry(dir.toString()));
-            for (ZipEntry zi: arrayList){
-                zipOutputStream.putNextEntry(zi);
+        ) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                int counter;
+                StringBuffer buffer = new StringBuffer();
+                while ((counter = zipInputStream.read()) != -1) {
+                    buffer.append((char) counter);
+                }
+                mapZipEntry.put(zipEntry, buffer);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(args[1]));
+        ) {
+            File file = new File(args[0]);
+            String fileName = "new/" + file.getName();
+            if (file.isFile()) {
+                zipOutputStream.putNextEntry(new ZipEntry(fileName));
+                Files.copy(file.toPath(), zipOutputStream);
+            }
+            for (Map.Entry<ZipEntry, StringBuffer> pair : mapZipEntry.entrySet()) {
+                if (!pair.getKey().getName().equals(file.getName())) {
+                    zipOutputStream.putNextEntry(pair.getKey());
+                    for (char c : pair.getValue().toString().toCharArray()) {
+                        zipOutputStream.write(c);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
