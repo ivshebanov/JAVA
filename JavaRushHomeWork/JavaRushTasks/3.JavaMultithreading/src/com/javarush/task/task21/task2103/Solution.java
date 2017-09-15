d9e7ddeec7a55fd909629d97fe8716b82bf5c1cb
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-15 11:12:07 +0300

    2103

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task21.task2103;
 
 /* 
 Все гениальное - просто!
+return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
 */
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
@@ -9,6 +10,9 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
+        System.out.println(calculate(true, true, true, false));
+        System.out.println(calculate(true, false, true, false));
+        System.out.println(calculate(false, false, true, false));
+        System.out.println(calculate(false, false, false, false));
     }
 }
