5146f8c3b5860d64d2765a42602f0b245c9711b3
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-15 10:50:43 +0300

    Игра змейка

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,46 +1,4 @@
 package com.javarush.task.task23.task2312;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Администратор on 24.03.2017.
- */
 public class Snake {
-    private List<SnakeSection> sections;
-    private boolean isAlive;
-    private SnakeDirection direction;
-
-    public Snake(int x, int y) {
-        this.sections = new ArrayList<SnakeSection>();
-        this.sections.add(new SnakeSection(x, y));
-        this.isAlive = true;
-    }
-
-    public void move(){
-
-    }
-    public int getX() {
-        return sections.get(0).getX();
-    }
-
-    public int getY() {
-        return sections.get(0).getY();
-    }
-
-    public List<SnakeSection> getSections() {
-        return sections;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public SnakeDirection getDirection() {
-        return direction;
-    }
-
-    public void setDirection(SnakeDirection direction) {
-        this.direction = direction;
-    }
 }
