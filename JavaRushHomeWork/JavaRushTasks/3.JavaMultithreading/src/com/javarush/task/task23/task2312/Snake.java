8d6ab2cc341fcf3980f60614112afdb8a60a1084
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-15 11:57:35 +0300

    Реализация змеи

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,4 +1,26 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+    private boolean isAlive;
+    private SnakeDirection direction;
+    private List<SnakeSection> sections = new ArrayList<SnakeSection>();
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
 }
