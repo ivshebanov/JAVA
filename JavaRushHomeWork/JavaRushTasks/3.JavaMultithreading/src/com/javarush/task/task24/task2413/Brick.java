f8c9de300d4b28b0a7e20d9caeaefb6f6b8fe257
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-19 16:36:26 +0300

    Арканоид(12) 2413

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -2,8 +2,8 @@ package com.javarush.task.task24.task2413;
 
 public class Brick extends BaseObject {
 
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
@@ -13,6 +13,6 @@ public class Brick extends BaseObject {
 
     @Override
     void move() {
-
+        //do nothing
     }
 }
