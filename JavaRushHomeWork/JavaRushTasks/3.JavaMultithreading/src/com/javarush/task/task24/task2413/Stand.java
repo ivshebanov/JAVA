19995ff10668d482da59f7adb07d741021a198f6
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-20 14:24:18 +0300

    Арканоид(16) 2413

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -2,8 +2,13 @@ package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject {
 
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
@@ -14,5 +19,13 @@ public class Stand extends BaseObject {
     @Override
     void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 }
