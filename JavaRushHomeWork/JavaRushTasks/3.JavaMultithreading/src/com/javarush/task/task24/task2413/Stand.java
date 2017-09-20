469554a02cdb167863b25aa2e5130e8932db67a9
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-20 14:37:53 +0300

    Арканоид(17) 2413

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -18,8 +18,18 @@ public class Stand extends BaseObject {
 
     @Override
     void move() {
-
+        x += speed * direction;
     }
+
+    public void moveLeft(){
+        direction = -1;
+    }
+
+    public void moveRight(){
+        direction = 1;
+    }
+
+
 
     public double getSpeed() {
         return speed;
