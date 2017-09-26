da70ddd34bafef9e271ba0dd61c4cb04b621a416
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-26 16:46:42 +0300

    setDefaultUncaughtExceptionHandler task 2510

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -5,9 +5,30 @@ package com.javarush.task.task25.task2510;
 */
 public class Solution extends Thread {
 
+    /**
+     * В конструкторе мы описываем обработчик исключений, который установлен для класса Solution.
+     */
     public Solution() {
+        setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                if (throwable instanceof Error) {
+                    System.out.println("Нельзя дальше работать");
+                } else if (throwable instanceof Exception) {
+                    System.out.println("Надо обработать");
+                } else {
+                    System.out.println("ХЗ");
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
+        new Solution().start();
+    }
+
+    @Override
+    public void run() {
+        throw new Error();
     }
 }
