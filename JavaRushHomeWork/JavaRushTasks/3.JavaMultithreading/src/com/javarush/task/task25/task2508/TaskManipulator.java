a6c751a8d994e0e68b530dba18d67a260259533d
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-26 10:52:44 +0300

    Изменения

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task25.task2508;
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+
+    private Thread thread;
+
+    @Override
+    public void run() {
+        System.out.println(thread.getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+}
