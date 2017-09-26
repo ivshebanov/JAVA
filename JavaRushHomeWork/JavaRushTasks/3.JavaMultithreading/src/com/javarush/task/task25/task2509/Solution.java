940430ec8ef5d5991c63b34a43983c545e8d69c3
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-26 16:09:12 +0300

    Added new task 2510

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2509/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2509/Solution.java
@@ -6,7 +6,8 @@ import java.util.concurrent.*;
 Все не так легко, как кажется
 */
 public class Solution extends ThreadPoolExecutor {
-    public Solution(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+    public Solution(int corePoolSize, int maximumPoolSize,
+                    long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
     }
 
