e2f467f49e723a72de527f4725958434c91844c4
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-28 13:51:55 +0300

    added new task2513

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -1,14 +1,38 @@
 package com.javarush.task.task25.task2512;
 
-/* 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+/*
 Живем своим умом
 */
 public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+
     }
 
     public static void main(String[] args) {
+        new Test(new Solution()).start();
+    }
+
+
+    static class Test extends Thread {
+        private Thread.UncaughtExceptionHandler handler;
+
+        public Test(Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
+            this.handler = uncaughtExceptionHandler;
+        }
+
+        @Override
+        public void run() {
+            this.handler.uncaughtException(Thread.currentThread(),
+                    new Exception("ABC",
+                            new RuntimeException("DEF",
+                                    new IllegalAccessException("GHI"))));
+        }
     }
 }
