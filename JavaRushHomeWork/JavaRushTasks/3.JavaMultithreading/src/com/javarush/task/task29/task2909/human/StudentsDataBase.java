e4c7ffbf8314751839bc2a02938c780d1d5df07f
Ilya Shebanov <ivshebanov@gmail.com>
2017-09-29 23:29:30 +0300

    solved task REFACTORING(8)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/StudentsDataBase.java
@@ -16,23 +16,24 @@ public class StudentsDataBase {
         System.out.println("Имя: " + student.getName() + " Возраст: " + student.getAge());
     }
 
-    public static void removeStudent(int index) throws IndexOutOfBoundsException {
-        students.remove(index);
+    public static void removeStudent(int index) {
+        try {
+            students.remove(index);
+        } catch (IndexOutOfBoundsException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static void findDimaOrSasha() {
-        boolean found = false;
-        for (int i = 0; i < students.size(); i++) {
-            if (!found) {
-                if (students.get(i).getName().equals("Dima")) {
-                    System.out.println("Студент Dima есть в базе.");
-                    found = true;
-                }
+        for (Student st : students) {
+            if (st.getName().equals("Dima")) {
+                System.out.println("Студент Dima есть в базе.");
+                break;
+            }
 
-                if (students.get(i).getName().equals("Sasha")) {
-                    System.out.println("Студент Sasha есть в базе.");
-                    found = true;
-                }
+            if (st.getName().equals("Sasha")) {
+                System.out.println("Студент Sasha есть в базе.");
+                break;
             }
         }
     }
