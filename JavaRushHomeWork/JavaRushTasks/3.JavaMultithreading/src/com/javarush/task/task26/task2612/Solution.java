eb0e39b8e07665961409c78c60910c9b35303bc9
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-04 10:58:36 +0300

    solved task2612 concurrent.Lock/ReentrantLock

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task26/task2612/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task26.task2612;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/* 
+Весь мир играет комедию
+*/
+public class Solution {
+    private Lock lock = new ReentrantLock();
+
+    public void someMethod() {
+        //implement logic here, use the lock field
+
+        if (!lock.tryLock()) {
+            ifLockIsBusy();
+        } else {
+            try {
+                ifLockIsFree();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void ifLockIsFree() {
+    }
+
+    public void ifLockIsBusy() {
+    }
+}
