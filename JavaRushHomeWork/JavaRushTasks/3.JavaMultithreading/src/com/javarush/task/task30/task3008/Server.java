d8434ccd7da3fed9b7ec3090b3b72bad0ed48605
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-08 14:27:10 +0300

    chat(7)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,13 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+
     public static void main(String[] args) {
         System.out.println("Введите порт: ");
         int port = ConsoleHelper.readInt();
@@ -12,7 +17,7 @@ public class Server {
         try {
             serverSocket = new ServerSocket(port);
         } catch (Exception e) {
-            System.out.println("Не создался ServerSocket");
+            ConsoleHelper.writeMessage("Не создался ServerSocket");
         }
         System.out.println("Сервер запушен!");
         assert serverSocket != null;
@@ -23,14 +28,13 @@ public class Server {
                 handler.start();
             }
         } catch (Exception e) {
-            System.out.println("Не создался Socket");
+            ConsoleHelper.writeMessage("Не создался Socket");
             try {
                 serverSocket.close();
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
         }
-
     }
 
     private static class Handler extends Thread {
@@ -44,5 +48,16 @@ public class Server {
         public void run() {
             super.run();
         }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        try {
+            for (Map.Entry<String, Connection> pair : connectionMap.entrySet()) {
+                pair.getValue().send(message);
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Сообщение не отправено!");
+        }
+
     }
 }
