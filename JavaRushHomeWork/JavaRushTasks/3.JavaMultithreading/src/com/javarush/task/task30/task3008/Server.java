e58c14390aa0c5c2183b290266f0803c262d104a
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-08 14:02:39 +0300

    chat(6) Server

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,48 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        System.out.println("Введите порт: ");
+        int port = ConsoleHelper.readInt();
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (Exception e) {
+            System.out.println("Не создался ServerSocket");
+        }
+        System.out.println("Сервер запушен!");
+        assert serverSocket != null;
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (Exception e) {
+            System.out.println("Не создался Socket");
+            try {
+                serverSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
 
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
     }
 }
