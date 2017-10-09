6f1bc88c101e583bd9a9b5f72b39527b86ebfd31
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-09 12:15:34 +0300

    Chat(6)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task30.task3008;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
-        
+        ConsoleHelper.writeMessage("Введите порт.");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Сервер запущен.");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Ошибка подключения.");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
     }
 }
