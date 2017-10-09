cd6fe0ccaa13869ed0ac863c5673ae04f5e1b31c
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-09 11:03:25 +0300

    Chat(3)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,5 +1,35 @@
 package com.javarush.task.task30.task3008;
 
-public class ConsoleHelper {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                writeMessage("Произошла ошибка при попытке ввода текста. " +
+                        "Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString());
+            } catch (NumberFormatException ex) {
+                writeMessage("Произошла ошибка при попытке ввода числа. " +
+                        "Попробуйте еще раз.");
+            }
+        }
+    }
 }
