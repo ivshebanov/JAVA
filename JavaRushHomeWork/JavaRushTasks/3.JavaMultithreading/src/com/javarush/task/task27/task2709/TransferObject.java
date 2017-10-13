7e65daf173c8506faefb460fef4ab41bea66e4d1
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-13 15:52:43 +0300

    task2709, wait-notify, Producer–consumer

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2709/TransferObject.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2709/TransferObject.java
@@ -5,12 +5,30 @@ public class TransferObject {
     protected volatile boolean isValuePresent = false; //use this variable
 
     public synchronized int get() {
+        while (!isValuePresent){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         System.out.println("Got: " + value);
+        isValuePresent = false;
+        this.notify();
         return value;
     }
 
     public synchronized void put(int value) {
+        while (isValuePresent){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         this.value = value;
         System.out.println("Put: " + value);
+        isValuePresent = true;
+        this.notify();
     }
 }
