79b5b9e11c44252f23386a12662088032d47f91a
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-14 18:57:25 +0300

    Archiver(3)

---
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        while (true) {
+            return bufferedReader.readLine();
+        }
+    }
+
+    public static int readInt() throws IOException {
+        while (true) {
+            return Integer.parseInt(bufferedReader.readLine());
+        }
+    }
+}
