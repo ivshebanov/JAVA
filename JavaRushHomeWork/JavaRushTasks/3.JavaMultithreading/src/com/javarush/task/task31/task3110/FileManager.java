6614fcc090e3e5833d4fe433713284fa02c27e65
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-16 19:25:41 +0300

    Archiver(9)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -1,19 +1,19 @@
 package com.javarush.task.task31.task3110;
 
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
     private Path rootPath;
     private List<Path> fileList;
 
-    FileManager(Path rootPath) throws IOException {
+    public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
-        fileList = new LinkedList<>();
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
 
@@ -22,14 +22,21 @@ public class FileManager {
     }
 
     private void collectFileList(Path path) throws IOException {
-        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (attrs.isRegularFile()) {
-                    fileList.add(rootPath.relativize(file));
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
-                return FileVisitResult.CONTINUE;
             }
-        });
+        }
     }
 }
