f3611193df42b96ad8c23a02dbac9f7f67acc3c4
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-25 15:59:13 +0300

    task 3209 HTML Editor (7.1)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/MenuHelper.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/MenuHelper.java
@@ -12,34 +12,42 @@ public class MenuHelper {
     }
 
     public static JMenuItem addMenuItem(JMenu parent, Action action) {
-        return addMenuItem(parent, null, action);
+        JMenuItem jMenuItem = new JMenuItem(action);
+        parent.add(jMenuItem);
+        return jMenuItem;
     }
 
-    public void initHelpMenu(View view, JMenuBar menuBar) {
-
+    public static JMenuItem addMenuItem(JMenu parent, String text, Action action) {
+        JMenuItem jMenuItem = addMenuItem(parent, action);
+        jMenuItem.setText(text);
+        return jMenuItem;
     }
 
-    public void initFontMenu(View view, JMenuBar menuBar) {
-
-    }
-
-    public void initColorMenu(View view, JMenuBar menuBar) {
-
-    }
-
-    public void initAlignMenu(View view, JMenuBar menuBar) {
+    public static void initHelpMenu(View view, JMenuBar menuBar) {
 
     }
 
-    public void initStyleMenu(View view, JMenuBar menuBar) {
+    public static void initFontMenu(View view, JMenuBar menuBar) {
 
     }
 
-    public void initEditMenu(View view, JMenuBar menuBar) {
+    public static void initColorMenu(View view, JMenuBar menuBar) {
 
     }
 
-    public void initFileMenu(View view, JMenuBar menuBar) {
+    public static void initAlignMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initStyleMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initEditMenu(View view, JMenuBar menuBar) {
+
+    }
+
+    public static void initFileMenu(View view, JMenuBar menuBar) {
 
     }
 }
