71043c899d4ca9f45a740e0c26069444d68dd3f2
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-30 22:24:47 +0300

    Resrtaurant (2)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,13 +1,7 @@
 package com.javarush.task.task27.task2712;
 
-import java.io.IOException;
-
 public class Restaurant {
     public static void main(String[] args) {
-        try {
-            ConsoleHelper.writeMessage(ConsoleHelper.getAllDishesForOrder().toString());
-        } catch (IOException e) {
-
-        }
+        new Tablet(5).createOrder();
     }
 }
--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -1,14 +1,28 @@
 package com.javarush.task.task27.task2712.kitchen;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
 import com.javarush.task.task27.task2712.Tablet;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Order {
     private final Tablet tablet;
     protected List<Dish> dishes;
 
-    public Order(Tablet tablet) {
+    public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
+        this.dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+
+    @Override
+    public String toString() {
+        if (!dishes.isEmpty())
+            return "Your order:" + dishes + "of Tablet" + tablet;
+        return "";
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
     }
 }
