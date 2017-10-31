e147f334032960beb9233e850ddcbd219c0cad4c
Ilya Shebanov <ivshebanov@gmail.com>
2017-10-31 22:40:56 +0300

    Restaurant(10)-1

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -1,12 +1,12 @@
 package com.javarush.task.task27.task2712.ad;
 
 public class Advertisement {
-    private long amountPerOneDisplaying;
-    private Object content;
+    private long amountPerOneDisplaying;//стоимость одного показа рекламного объявления
+    private Object content;//видео.
     private String name;
-    private long initialAmount;
-    private int hits;
-    private int duration;
+    private long initialAmount;//начальная сумма, стоимость рекламы в копейках.
+    private int hits;//количество оплаченных показов.
+    private int duration;//продолжительность в секундах.
 
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
@@ -29,9 +29,14 @@ public class Advertisement {
         return duration;
     }
 
-    public void revalidate(){
-        if (hits <= 0)
+    public void revalidate() {
+        if (hits <= 0) {
             throw new UnsupportedOperationException();
+        }
         hits--;
+    }
+
+    public int getHits() {
+        return hits;
     }
 }
--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,5 +1,12 @@
 package com.javarush.task.task27.task2712.ad;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -9,7 +16,95 @@ public class AdvertisementManager {
     }
 
     public void processVideos() throws NoVideoAvailableException {
-        if (storage.list().isEmpty())
+        List<Advertisement> availableVideos = storage.list();
+        if (availableVideos.isEmpty()) {
             throw new NoVideoAvailableException();
+        }
+
+        List<Advertisement> videoToBeShown = new ArrayList<>();
+        for (Advertisement ad : availableVideos) {
+            if (ad.getDuration() <= timeSeconds && ad.getHits() > 0) {
+                videoToBeShown.add(ad);
+            }
+        }
+
+        videoToBeShown = getVideos(videoToBeShown, 0);
+        if (videoToBeShown.isEmpty()) {
+            throw new NoVideoAvailableException();
+        }
+
+
+        Collections.sort(videoToBeShown, new Comparator<Advertisement>() {
+            @Override
+            public int compare(Advertisement o1, Advertisement o2) {
+                int result = Long.compare(o1.getAmountPerOneDisplaying(), o2.getAmountPerOneDisplaying());
+                if (result == 0)
+                    result = Long.compare(o1.getAmountPerOneDisplaying() / o1.getDuration(),
+                            o2.getAmountPerOneDisplaying() / o2.getDuration());
+                return result;
+            }
+        });
+        Collections.reverse(videoToBeShown);
+        for (Advertisement ad : videoToBeShown) {
+            ConsoleHelper.writeMessage(String.format("%s  is displaying... %d, %d",
+                    ad.getName(),
+                    ad.getAmountPerOneDisplaying(),
+                    ad.getAmountPerOneDisplaying() * 1000 / ad.getDuration()));
+            ad.revalidate();
+        }
+
+    }
+
+    public List<Advertisement> getVideos(List<Advertisement> inList, int start) {
+        List<Advertisement> storageList = storage.list();
+        List<Advertisement> tmp;
+        List<Advertisement> result = new ArrayList<>();
+
+        int timeLeft = timeSeconds - sumOfTime(inList);
+
+        Advertisement ad;
+
+        for (int i = start; i < storageList.size(); i++) { // проходим по сторейдж массиву
+            ad = storageList.get(i);
+            if (!inList.contains(ad) && ad.getHits() > 0 && (ad.getDuration() <= timeLeft)) {
+                inList.add(ad);
+                result = checkWhoBetter(inList, getVideos(inList, i));
+            }
+        }
+        return result;
+    }
+
+    // выводит сумму прибыли за показ листа
+    public int sumOfListMoney(List<Advertisement> list) {
+        int sum = 0;
+        for (Advertisement ad : list) {
+            sum += ad.getAmountPerOneDisplaying();
+        }
+        return sum;
+    }
+
+    // возвращает суммарную длительность роликов в листе
+    public int sumOfTime(List<Advertisement> list) {
+        int summ = 0;
+        for (Advertisement ad : list) {
+            summ += ad.getDuration();
+        }
+        return summ;
+    }
+
+    //сравнивает два листа по требованиям
+    public List<Advertisement> checkWhoBetter(List<Advertisement> check, List<Advertisement> tmp) {
+        List<Advertisement> result = new ArrayList<>();
+        if (sumOfListMoney(check) > sumOfListMoney(tmp)) result = check;
+        if (sumOfListMoney(check) < sumOfListMoney(tmp)) result = tmp;
+        if (sumOfListMoney(check) == sumOfListMoney(tmp)) {
+            if (sumOfTime(check) > sumOfTime(tmp)) result = check;
+            if (sumOfTime(check) < sumOfTime(tmp)) result = tmp;
+            if (sumOfTime(check) == sumOfTime(tmp)) {
+                if (check.size() > tmp.size()) result = check;
+                else result = tmp;
+            }
+        }
+        return result;
     }
 }
