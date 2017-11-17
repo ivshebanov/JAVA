5cd017262a256dd9ee95dd8eb90dd7a483c14189
Ilya Shebanov <ivshebanov@gmail.com>
2017-11-17 16:46:07 +0300

    2048 (5)

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    public int score; //текущий счет
+    public int maxTile; //макс вес плитки
 
     public Model() {
         resetGameTiles();
@@ -29,7 +31,7 @@ public class Model {
     private void addTile() {
         List<Tile> emptyTiles = getEmptyTiles();
         int randomEmptyTile = (int) (Math.random() * emptyTiles.size());
-        if (!emptyTiles.isEmpty()){
+        if (!emptyTiles.isEmpty()) {
             emptyTiles.get(randomEmptyTile).setValue(Math.random() < 0.9 ? 2 : 4);
         }
     }
@@ -44,5 +46,36 @@ public class Model {
             }
         }
         return emptyTiles;
+    }
+
+    public Tile[] compressTiles(Tile[] tiles) {
+        for (int i = 0; i < tiles.length - 1; i++) {
+            if (tiles[i].getValue() == 0) {
+                for (int j = i; j < tiles.length - 1; j++) {
+                    tiles[j].setValue(tiles[j + 1].value);
+                }
+                tiles[tiles.length - 1].setValue(0);
+            }
+        }
+        return tiles;
+    }
+
+    public void mergeTiles(Tile[] tiles) {
+        for (int i = 0; i < tiles.length - 1; i++) {
+            if (tiles[i].getValue() == tiles[i + 1].getValue()) {
+                tiles[i].setValue(tiles[i].value * 2);
+                tiles[i + 1].setValue(0);
+                compressTiles(tiles);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < tiles.length; i++) {
+            System.out.print(tiles[i].getValue());
+        }
+    }
+
+
+    public void mergeTiles() {
+
     }
 }
