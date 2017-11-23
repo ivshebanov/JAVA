de9831c6ec0861224afd90fd6410c041690d863b
Ilya Shebanov <ivshebanov@gmail.com>
2017-11-23 12:33:45 +0300

    2048 (8)-1

--- JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushHomeWork/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -143,23 +143,22 @@ public class Model {
         System.arraycopy(newTiles, 0, gameTiles, 0, newTiles.length);
     }
 
-    private boolean canMove() {
-
+    public boolean canMove() {
         if (!getEmptyTiles().isEmpty()){
             return true;
         }
 
         for (int i = 0; i < gameTiles[0].length; i++) {
-            for (int j = 0; j < gameTiles.length - 1; j++) {
-                if (gameTiles[i][j].getValue() == gameTiles[i][j + 1].getValue()) {
+            for (int j = 1; j < gameTiles.length; j++) {
+                if (gameTiles[i][j].getValue() == gameTiles[i][j - 1].getValue()) {
                     return true;
                 }
             }
         }
 
-        for (int i = 0; i < gameTiles[0].length - 1; i++) {
-            for (int j = 0; j < gameTiles.length; j++) {
-                if (gameTiles[i][j].getValue() == gameTiles[i + 1][j].getValue()) {
+        for (int j = 0; j < gameTiles.length; j++) {
+            for (int i = 1; i < gameTiles[0].length; i++) {
+                if (gameTiles[i][j].getValue() == gameTiles[i - 1][j].getValue()) {
                     return true;
                 }
             }
