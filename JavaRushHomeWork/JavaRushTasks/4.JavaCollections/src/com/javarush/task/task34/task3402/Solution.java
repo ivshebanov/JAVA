84ed33774a2aff5ae66213dc9a96ebd41c0fd2cf
Ilya Shebanov <ivshebanov@gmail.com>
2017-12-22 16:48:38 +0300

    task3402 recursive Factorial_2

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
 
     public int factorial(int n) {
         if (n == 0) {
-            return 0;
+            return 1;
         }
         if (n == 1) {
             return 1;
