7528bf7bcc49316bb0cae9118f60da749b87175b
Ilya Shebanov <ivshebanov@gmail.com>
2018-02-20 11:35:23 +0300

    task3408 reflection, cache, WeakHashMap

---
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3408/Cache.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task34.task3408;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class Cache<K, V> {
+    private Map<K, V> cache = new WeakHashMap<>();   //TODO add your code here
+
+    public V getByKey(K key, Class<V> clazz) throws Exception {
+        //TODO add your code here
+        if (!cache.containsKey(key)) {
+            cache.put(key, clazz.getConstructor(key.getClass()).newInstance(key));
+        }
+
+        return cache.get(key);
+    }
+
+    public boolean put(V obj) {
+        //TODO add your code here
+        Method method = null;
+        try {
+            method = obj.getClass().getDeclaredMethod("getKey");
+            method.setAccessible(true);
+            K key = (K) method.invoke(obj);
+            cache.put(key, obj);
+            return cache.containsKey(key);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ignore) {
+
+        }
+        return false;
+    }
+
+    public int size() {
+        return cache.size();
+    }
+}
