8906e812607b1c4de5c4f6cde3b2a043e089130f
Ilya Shebanov <ivshebanov@gmail.com>
2018-03-29 14:11:42 +0300

    task0918 Exception

--- JavaRushHomeWork/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0918/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0918/Solution.java
@@ -4,20 +4,23 @@ package com.javarush.task.task09.task0918;
 Все свои, даже исключения
 */
 
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) {
-    }
-    
-    static class MyException {
+
     }
 
-    static class MyException2 extends MyException {
+    static class MyException extends RuntimeException {
     }
 
-    static class MyException3 {
+    static class MyException2 extends Error {
     }
 
-    static class MyException4 {
+    static class MyException3 extends ReflectiveOperationException {
+    }
+
+    static class MyException4 extends IOException {
     }
 }
 
