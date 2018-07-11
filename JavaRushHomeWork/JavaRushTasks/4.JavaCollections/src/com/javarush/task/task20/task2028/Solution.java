d3f81332c2802aca30a08ebe0330e63c359abd7f
Ilya Shebanov <ivshebanov@gmail.com>
2018-07-11 16:55:37 +0300

    Сброс задачи

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -5,32 +5,5 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
-
-        for (int i = 1; i < 16; i++) {
-            list.add(String.valueOf(i));
-        }
-
-        CustomTree.printTree();
-        System.out.println();
-
-        System.out.println("List size is " + list.size());
-        System.out.println("Expected parent is 3, actual parent is " + ((CustomTree) list).getParent("8"));
-        System.out.println("Expected parent is null, actual parent is " + ((CustomTree) list).getParent("20"));
-
-        list.remove("3");
-        CustomTree.printTree();
-        System.out.println();
-        System.out.println("Expected parent is null, actual parent is " + ((CustomTree) list).getParent("8"));
-
-        list.add("16");
-        CustomTree.printTree();
-        System.out.println();
-        System.out.println("Expected parent is 9, actual parent is " + ((CustomTree) list).getParent("16"));
-
-        list.remove("4");
-        list.remove("5");
-        list.remove("6");
-        System.out.println("Expected true, actual " + list.add("20"));
-        System.out.println("Expected parent is 1, actual parent is " + ((CustomTree) list).getParent("20"));
     }
 }
