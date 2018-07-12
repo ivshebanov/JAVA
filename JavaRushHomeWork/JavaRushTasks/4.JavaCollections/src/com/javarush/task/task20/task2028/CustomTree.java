3332f3ac1be25d250262a7e425d4421c892751ae
Ilya Shebanov <ivshebanov@gmail.com>
2018-07-12 10:59:37 +0300

    task2028 tree (1)

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,10 +1,20 @@
 package com.javarush.task.task20.task2028;
 
-import java.util.List;
+import java.io.Serializable;
+import java.util.AbstractList;
 
 /* 
 Построй дерево(1)
 */
-public class CustomTree {
+public class CustomTree extends AbstractList<String> implements Serializable, Cloneable {
 
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
