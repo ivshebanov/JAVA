a7f2f023c0c5354b1396bde1f98a87419e80fac5
Ilya Shebanov <ivshebanov@gmail.com>
2018-07-17 12:30:17 +0300

    task 3309 xml

---
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task33.task3309;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.StringWriter;
+
+/*
+Комментарий внутри xml
+*/
+public class Solution {
+    public static String toXmlWithComment(Object obj, String tagName, String comment) {
+        StringWriter sw = new StringWriter();
+        try {
+            Marshaller marshaller = JAXBContext.newInstance(First.class, Second.class).createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(obj, sw);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        String result = sw.toString();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        First firstSecondObject = new First();
+
+        System.out.println(toXmlWithComment(firstSecondObject, "second", "it's a comment"));
+
+    }
+
+    @XmlRootElement(name = "first")
+    public static class First {
+        @XmlElement(name = "second")
+        public String item1 = "some string";
+        @XmlElement(name = "second")
+        public String item2 = "need CDATA because of <second>";
+        @XmlElement(name = "second")
+        public String item3 = "";
+        @XmlElement(name = "third")
+        public String item4;
+        @XmlElement(name = "forth")
+        public Second[] third = new Second[]{new Second()};
+        @XmlElement(name = "fifth")
+        public String item5 = "need CDATA because of <";
+    }
+
+    public static class Second {
+        @XmlElement(name = "second")
+        public String item1 = "some string";
+        @XmlElement(name = "second")
+        public String item2 = "need CDATA because of <second>";
+    }
+}
