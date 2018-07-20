a16a4a7d0861f37f2eaead618cd21719c01812a6
Ilya Shebanov <ivshebanov@gmail.com>
2018-07-20 15:25:54 +0300

    task3310 Shortener (5)

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
@@ -1,7 +1,14 @@
 package com.javarush.task.task36.task3610;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 public class MyMultiMap<K, V> extends HashMap<K, V> implements Cloneable, Serializable {
     static final long serialVersionUID = 123456789L;
