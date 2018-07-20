071275aa77d398f90eb86a0d98b311ba5e32e4d3
Ilya Shebanov <ivshebanov@gmail.com>
2018-07-20 15:01:47 +0300

    task3310 Shortener (4)

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task33.task3310;
 
 public class Solution {
     public static void main(String[] args) {
-
+        for (int i = 0; i<20; i++){
+            Helper.printMessage(Helper.generateRandomString());
+        }
     }
 }
