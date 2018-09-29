7b8be0952b89c8120cc0155d1175113b29058caa
Ilya Shebanov <ivshebanov@gmail.com>
2018-09-29 14:03:19 +0300

    task3811 Своя аннотация - 2

---
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task38/task3811/Ticket.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task38.task3811;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Ticket {
+    Priority priority();
+
+    String createdBy();
+
+    String[] tags();
+
+    enum Priority {
+        LOW,
+        MEDIUM,
+        HIGH;
+    }
+}
