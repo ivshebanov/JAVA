c8167c390cd6237b0a1b9d51085d221df77ae448
Ilya Shebanov <ivshebanov@gmail.com>
2018-09-30 14:50:17 +0300

    task 2810 Aggregator - 4 Формат URL

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,4 +1,8 @@
 package com.javarush.task.task28.task2810.model;
 
 public class HHStrategy implements Strategy {
+    //"http://hh.ua/search/vacancy?text=java+Kiev&page=3"
+
+    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+    String url = String.format(URL_FORMAT, "Kiev", 3);
 }
