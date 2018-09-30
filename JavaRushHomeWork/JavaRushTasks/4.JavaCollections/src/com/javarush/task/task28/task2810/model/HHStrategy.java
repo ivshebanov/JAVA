4693f91f4658915fe951d466a9b9ab6939745b64
Ilya Shebanov <ivshebanov@gmail.com>
2018-09-30 16:02:05 +0300

    task 2810 Aggregator - 6

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,18 +1,25 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
-    String url = String.format(URL_FORMAT, "Kiev", 3);
-
+    String URL = String.format(URL_FORMAT, "Kiev", 3);
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         List<Vacancy> resultList = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return resultList;
     }
 }
