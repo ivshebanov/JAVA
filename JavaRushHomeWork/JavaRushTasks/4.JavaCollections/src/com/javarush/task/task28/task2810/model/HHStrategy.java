dbfce4a24bbc75b02589fece76eeefd682430fa3
Ilya Shebanov <ivshebanov@gmail.com>
2018-09-30 16:35:13 +0300

    task 2810 Aggregator - 7

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -10,16 +10,26 @@ import java.util.List;
 
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
-    String URL = String.format(URL_FORMAT, "Kiev", 3);
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         List<Vacancy> resultList = new ArrayList<>();
+        int pageNumber = 0;
         try {
-            Document doc = Jsoup.connect(URL).get();
+            Document doc = getDocument(searchString, pageNumber);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return resultList;
+    }
+
+    protected Document getDocument(String searchString, int pageNumber) throws IOException {
+        String url = String.format(URL_FORMAT, searchString, pageNumber);
+        return Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
+                .referrer("https://hh.ru/search/vacancy?text=java+%D0%BA%D0%B8%D0%B5%D")
+                .timeout(5000)
+                .get();
+
     }
 }
