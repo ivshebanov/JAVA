08522c35cfa13adc1e10a5de72dbff32ba773a9f
Ilya Shebanov <ivshebanov@gmail.com>
2018-10-06 15:38:47 +0300

    task2810 Рефакторинг контроллера и маин

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,41 +1,19 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.task.task28.task2810.model.Model;
 
 public class Controller {
 
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers == null || providers.length == 0) {
+    public Controller(Model model) {
+        if (model == null) {
             throw new IllegalArgumentException();
         }
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + (providers == null ? null : Arrays.asList(providers)) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> resultList = new ArrayList<>();
-
-        for (Provider provider : providers) {
-            try {
-                resultList.addAll(provider.getJavaVacancies("Москва"));
-            } catch (NullPointerException e) {
-                System.out.println("NPE");
-                e.getStackTrace();
-            }
-        }
-        System.out.println(resultList.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
