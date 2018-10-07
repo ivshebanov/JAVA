5d2281ee67e1a8ef2982358c1a1c4aa7429cd98d
Ilya Shebanov <ivshebanov@gmail.com>
2018-10-07 14:02:00 +0300

    task2810 Реализация view

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 public class HtmlView implements View {
 
+    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replaceAll("\\.", "/") + "/vacancies.html";
+//    private final String filePath = "/Users/iliashebanov/Documents/Java/JavaRush/JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/view/vacancies.html";
+
     private Controller controller;
 
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        updateFile(getUpdatedFileContent(vacancies));
     }
 
     @Override
@@ -21,5 +24,13 @@ public class HtmlView implements View {
 
     public void userCitySelectEmulationMethod() {
         controller.onCitySelect("Moscow");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+        return "";
+    }
+
+    private void updateFile(String vacancy) {
+
     }
 }
