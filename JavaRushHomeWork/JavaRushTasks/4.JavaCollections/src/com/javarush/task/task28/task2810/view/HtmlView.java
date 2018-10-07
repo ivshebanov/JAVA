aa6ae2514b5b41a36554bba8ec1f4237a82e6779
Ilya Shebanov <ivshebanov@gmail.com>
2018-10-07 17:25:49 +0300

    task2810 Реализовал getUpdatedFileContent

--- JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ JavaRushHomeWork/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,6 +2,9 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -29,8 +32,37 @@ public class HtmlView implements View {
         controller.onCitySelect("Moscow");
     }
 
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
+    }
+
     private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return "";
+        Document document = null;
+        try {
+            document = getDocument();
+
+            Element templateOriginal = document.getElementsByClass("template").first();
+            Element copyTemplate = templateOriginal.clone();
+            copyTemplate.removeAttr("style");
+            copyTemplate.removeClass("template");
+            document.select("tr[class=vacancy]").remove();
+
+            for (Vacancy vacancy : vacancies) {
+                Element localClone = copyTemplate.clone();
+                localClone.getElementsByClass("city").first().text(vacancy.getCity());
+                localClone.getElementsByClass("companyName").first().text(vacancy.getCompanyName());
+                localClone.getElementsByClass("salary").first().text(vacancy.getSalary());
+                Element link = localClone.getElementsByTag("a").first();
+                link.text(vacancy.getTitle());
+                link.attr("href", vacancy.getUrl());
+
+                templateOriginal.before(localClone.outerHtml());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
+        return document.html();
     }
 
     private void updateFile(String vacancy) {
